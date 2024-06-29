2866466c9ca53d51dcffc82a8c7e0c872c233452
Kazakbaev Ilim <ilimkgz777@gmail.com>
2024-06-29 14:19:34 +0600

    added type for send data, and routes for pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,21 @@
 import Toolbar from './components/Toolbar/Toolbar.tsx';
+import {Route, Routes} from 'react-router-dom';
+import Quotes from './containers/Quotes/Quotes.tsx';
+import QuotesItem from './components/QuotesItem/QuotesItem.tsx';
+import AddQuoteMutation from './containers/addQuoteMutation/AddQuoteMutation.tsx';
 
 const App = () => (
   <>
       <header className={'container-md mt-3'}>
         <Toolbar/>
       </header>
-      <main></main>
+      <main className={'container-md mt-3'}>
+        <Routes>
+          <Route path="/" element={<Quotes/>}/>
+          <Route path="/quotes/:id" element={<QuotesItem/>}/>
+          <Route path={'add-quote'} element={<AddQuoteMutation/>}/>
+        </Routes>
+      </main>
   </>
 );
 
--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import {BrowserRouter} from 'react-router-dom';
+import {SnackbarProvider} from 'notistack';
+
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
 
   <React.StrictMode>
     <BrowserRouter>
+      <SnackbarProvider/>
       <App/>
     </BrowserRouter>
   </React.StrictMode>,
